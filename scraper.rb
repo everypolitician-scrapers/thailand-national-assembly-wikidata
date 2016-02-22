967552acaa42ce290401a08351b319e451309972
Tony Bowden <tony@tmtm.com>
2016-02-22 16:59:01 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -8,8 +8,7 @@ require 'scraperwiki'
 require 'wikidata/fetcher'
 require 'mediawiki_api'
 
-
-def members
+def names
   morph_api_url = 'https://api.morph.io/davewhiteland/thailand-national-assembly/data.json'
   morph_api_key = ENV["MORPH_API_KEY"]
   result = RestClient.get morph_api_url, params: {
@@ -19,15 +18,5 @@ def members
   JSON.parse(result, symbolize_names: true)
 end
 
-WikiData.ids_from_pages('th', members.map { |c| c[:wikiname] }).each_with_index do |p, i|
-  data = WikiData::Fetcher.new(id: p.last).data('th') rescue nil
-  unless data
-    warn "No data for #{p}"
-    next
-  end
-  data[:original_wikiname] = p.first
-  ScraperWiki.save_sqlite([:id], data)
-end
-
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
-
+EveryPolitician::Wikidata.scrape_wikidata(names: { th: names.map { |n| n[:wikiname] } }, output: true)
+warn EveryPolitician::Wikidata.notify_rebuilder
