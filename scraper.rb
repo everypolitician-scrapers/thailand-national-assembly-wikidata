0ba2f728ab2573db777b7a41ea21af7fd41679e5
Tony Bowden <tony@tmtm.com>
2016-02-24 07:18:31 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,22 +1,7 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'json'
-require 'pry'
-require 'rest-client'
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'mediawiki_api'
 
-def names
-  morph_api_url = 'https://api.morph.io/davewhiteland/thailand-national-assembly/data.json'
-  morph_api_key = ENV["MORPH_API_KEY"]
-  result = RestClient.get morph_api_url, params: {
-    key: morph_api_key,
-    query: "select DISTINCT(name) AS wikiname from wikinames"
-  }
-  JSON.parse(result, symbolize_names: true)
-end
-
-EveryPolitician::Wikidata.scrape_wikidata(names: { th: names.map { |n| n[:wikiname] } }, output: false)
-warn EveryPolitician::Wikidata.notify_rebuilder
+names = EveryPolitician::Wikidata.morph_wikinames(source: 'davewhiteland/thailand-national-assembly', column: 'name')
+EveryPolitician::Wikidata.scrape_wikidata(names: { th: names }, output: false)
